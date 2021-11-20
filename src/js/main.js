21dee439b27f366cf51326f99ad7fac85e238f79
Minh-Trung-SE <nguyenminhtrungse@gmail.com>
2021-11-20 21:42:14 +0700

    First commit -m

---
+++ src/js/main.js
@@ -0,0 +1,32 @@
+function handleShowProject(project){
+    let currentImg = 0;
+    let numberImg = 0;
+    
+    let listImg = project.querySelectorAll(".project-item__img");
+    listImg = Array.from(listImg);
+    numberImg = listImg.length;
+
+    console.log(listImg);
+    listImg[currentImg].classList.add('active');
+    setInterval(() => {
+        if(listImg[currentImg].classList.contains('active')){
+            listImg[currentImg].classList.remove('active');
+        }
+        
+        currentImg = ++currentImg;
+        if(currentImg >= numberImg){
+            currentImg = 0;
+        }
+        listImg[currentImg].classList.add('active');
+    
+    }, 3000)
+}
+
+function slideShowProject(){
+    let listProjectDemo = document.querySelectorAll('.project-item__list-img');
+    listProjectDemo = Array.from(listProjectDemo);
+    listProjectDemo.forEach((project) => handleShowProject(project));
+}
+
+    
+slideShowProject();
